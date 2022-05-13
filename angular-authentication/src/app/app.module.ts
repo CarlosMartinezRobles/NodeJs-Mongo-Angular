46ab6ac75cac7c2003e8e37f8f97ea499ec267c4
CarlosMartinezRobles <90333573+CarlosMartinezRobles@users.noreply.github.com>
2022-05-13 10:12:01 +0200

    compila bien, falta formulario

--- angular-authentication/src/app/app.module.ts
+++ angular-authentication/src/app/app.module.ts
@@ -15,9 +15,11 @@ import { MenuComponent } from './core/components/menu/menu.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    
+  ],
+  providers: [
     FormGroup,
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- angular-authentication/src/app/features/register/register.module.ts
+++ angular-authentication/src/app/features/register/register.module.ts
@@ -17,25 +17,3 @@ import { ReactiveFormsModule } from '@angular/forms';
   ]
 })
 export class RegisterModule { }
-// import { NgModule } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-
-
-// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-// import { RegisterComponent } from './components/register/register.component';
-// import { RegisterRoutingModule } from './register-routing.module';
-
-
-
-// @NgModule({
-//   declarations: [
-//     RegisterComponent
-//   ],
-//   imports: [
-//     CommonModule,
-//     RegisterRoutingModule,
-//     ReactiveFormsModule,
-//     FormsModule
-//   ]
-// })
-// export class SingupModule { }
